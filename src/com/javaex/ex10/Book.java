659a2877f9f20952e54dd1b45a08ea1476b339ae
H-JUYEONG <dud9902@gmail.com>
2024-07-04 00:00:03 +0900

    수정

--- src/com/javaex/ex10/Book.java
+++ src/com/javaex/ex10/Book.java
@@ -17,20 +17,6 @@ public class Book {
 	}
 
 	// 메소드
-	public void rent() {
-		this.stateCode = 0;
-		System.out.println(this.title + "이(가) 대여 됐습니다.");
-	}
-
-	public void print() {
-		if (this.stateCode == 1) {
-			System.out.println(this.bookNo + " 책 제목:" + this.title + ", 작가:" + this.author + ", 대여유무: 재고있음");
-		} else {
-			System.out.println(this.bookNo + " 책 제목:" + this.title + ", 작가:" + this.author + ", 대여유무: 대여중");
-		}
-		System.out.println();
-	}
-
 	public void setBookNo(int bookNo) {
 		this.bookNo = bookNo;
 	}
@@ -61,6 +47,19 @@ public class Book {
 
 	public int getStateCode() {
 		return stateCode;
+	}
+
+	public void rent() {
+		this.stateCode = 0;
+		System.out.println(this.title + "이(가) 대여 됐습니다.");
+	}
+
+	public void print() {
+		if (this.stateCode == 1) {
+			System.out.println(this.bookNo + " 책 제목:" + this.title + ", 작가:" + this.author + ", 대여유무: 재고있음");
+		} else {
+			System.out.println(this.bookNo + " 책 제목:" + this.title + ", 작가:" + this.author + ", 대여유무: 대여중");
+		}
 	}
 
 }
